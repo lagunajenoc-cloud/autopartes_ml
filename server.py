0df7ecc62ec25953b0304e05a66f8dbdba5ad5c5
PROYECTO <elagunajaramillo@gmail.com>
2026-05-07 20:45:35 -0500

    s

--- server.py
+++ server.py
@@ -1,3 +1,12 @@
 from app import create_app
 
 app = create_app()
+
+# ------ Ruta necesaria para Railway (health check) ------
+@app.route("/")
+def health_check():
+    return "OK", 200
+# --------------------------------------------------------
+
+if __name__ == "__main__":
+    app.run()
